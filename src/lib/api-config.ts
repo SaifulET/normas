aeba8a8b5e1c47d088303c0f9baf27a7b4515256
Saiful <si912999@gmail.com>
2026-06-01 00:48:15 +0600

    link change

--- src/lib/api-config.ts
+++ src/lib/api-config.ts
@@ -1,3 +1,4 @@
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://normas-backend.vercel.app/api/v1/";
+  process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://normas-backend-yy87.vercel.app/api/v1/";
+  // process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://normas-backend.vercel.app/api/v1/";
   // process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:5000/api/v1/";
